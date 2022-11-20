e4f348a5bc8c52e0ad091a8a8ac515df9b7d3882
Daria.Karabaeva@urfu.me <Daria.Karabaeva@urfu.me>
2022-11-20 21:24:34 +0500

    HW6

---
+++ hw8/hw8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw8
+{
+    internal class Program
+    {
+       public double FunctUndetZeroOrZero()
+        {
+            return Math.Exp((x + 1));
+        }
+        public double FunctBetweenMinOneAndZeroOrZero()
+        {
+         return 1;
+        }
+        public double FunctOverZero()
+        {
+            return Math.Exp( - x);
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите аргумент функции");
+            double x = Convert.ToDouble(Console.ReadLine());
+            var pr = new Program();
+            if (x <= -1)
+                Console.WriteLine("Значение функции = " + pr.FunctUndetZeroOrZero());
+            else if (x > 0)
+                Console.WriteLine("Значение функции = " + pr.FunctOverZero());
+            else
+                Console.WriteLine("Значение функции = " + pr.FunctBetweenMinOneAndZeroOrZero());
+            Console.ReadKey();
+
+
+
+        }
+            
+
+
+    }
+
+}
