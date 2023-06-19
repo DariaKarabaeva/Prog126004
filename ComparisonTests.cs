cd25d903103a19e2f6fb73d99f1938d8294ac9e5
DariaKarabaeva <114229609+DariaKarabaeva@users.noreply.github.com>
2023-06-19 23:56:08 +0500

    домашнее задание 14

---
+++ ComparisonTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+
+[TestFixture]
+public class IComparableTests
+{
+    private static Edition edition1;
+    private static Edition edition2;
+    private static Edition edition3;
+    private static EditionComparer comparer;
+    private static Edition[] editions;
+    private static Section section;
+    
+    [SetUp]
+    public void Setup()
+    {
+        edition1 = new Edition("1234", new List<string> {"aaa", "b"}, 2020, "publisher", 2);
+        edition2 =  new Edition("1232", new List<string> {"aab"}, 2021, "publisher", 1);
+        edition3 =  new Edition("1234", new List<string> {"aaa"}, 2022, "publisher", 3);
+        comparer = new EditionComparer();
+        editions = new Edition[] { edition1, edition2, edition3 };
+        section = new Section(editions);
+    }
+
+    [Test]
+    public void CompareToTest()
+    {
+        Assert.That(edition1.CompareTo(edition2), Is.LessThan(0));
+        Assert.That(edition1.CompareTo(edition3), Is.GreaterThan(0));
+    }
+
+    [Test]
+    public void ComparerTest()
+    {
+        Assert.That(comparer.Compare(edition1, edition2), Is.GreaterThan(0));
+        Assert.That(comparer.Compare(edition1, edition3), Is.LessThan(0));
+    }
+
+    [Test]
+    public void CatalogueCountTest()
+    {
+        Assert.That(section.Count, Is.EqualTo(3));
+    }
+    
+    [Test]
+    public void IEnumerableTest()
+    {
+        var i = 0;
+        foreach(var member in section)
+            Assert.That(member, Is.SameAs(editions[i++]));
+    }
+}
---
+++ Edition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Edition : IComparable<Edition>
+{
+    public string Name { get; set; }
+    public List<string> Authors { get; set; }
+    public int Year { get; set; }
+    public string Publisher  { get; set; }
+    public readonly int InventoryNumber;
+    public EditionStatus Status;
+    public int Price;
+    
+    public Edition() {}
+
+    public Edition(string name, List<string> authors, int year, string publisher, int inventoryNumber)
+    {
+        Name = name;
+        Authors = authors;
+        Year = year;
+        Publisher = publisher;
+        InventoryNumber = inventoryNumber;
+    }
+
+    public string GetStatus()
+    {
+        if (Status == EditionStatus.ClientHome)
+            return "Выдана на дом";
+        if (Status == EditionStatus.ReadingRoom)
+            return "Выдана в читальный зал";
+        return "В хранилище";
+    }
+    
+    public virtual string GetInfo()
+    {
+        var result = new StringBuilder();
+        if (Name != default)
+            result.Append($"Название: {Name}.");
+        if (Year != default)
+            result.Append($" Год: {Year}.");
+        if (Authors != default)
+            result.Append($" Авторы: {string.Join(',', Authors)}.");
+        if (Publisher != default)
+            result.Append($" Издательство: {Publisher}.");
+        if (InventoryNumber != default)
+            result.Append($" ИН: {InventoryNumber}.");
+        result.Append($" Статус: {GetStatus()}.");
+        if (Price != 0)
+            result.Append($" Цена: {Price}.");
+        return result.ToString();
+    }
+
+    public int CompareTo(Edition other)
+    {
+        var result = Authors.First().CompareTo(other.Authors.First());
+        if (result == 0)
+        {
+            result = Name.CompareTo(other.Name);
+            if (result == 0)
+                result = -Year.CompareTo(other.Year);
+        }
+        return result;
+    }
+}
